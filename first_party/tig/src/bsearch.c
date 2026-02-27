506eca2ec487fb52eb231c86e27233ce26983b60
Alexander Batalov <alex.batalov@gmail.com>
2026-02-27 08:38:22 +0300

    Review tig_bsearch implementation

--- first_party/tig/src/bsearch.c
+++ first_party/tig/src/bsearch.c
@@ -5,56 +5,22 @@
 // 0x537940
 void* tig_bsearch(void const* key, void const* base, size_t cnt, size_t size, int (*fn)(void const*, void const*), int* exists_ptr)
 {
-    unsigned char* l;
-    unsigned char* r;
+    unsigned char* l = (unsigned char*)base;
+    unsigned char* r = l + size * cnt;
     unsigned char* mid;
-    int index;
     int cmp;
 
-    l = (unsigned char*)base;
-    r = (unsigned char*)base + size * (cnt - 1);
-
-    while (l <= r) {
-        index = cnt / 2;
-        if (index == 0) {
-            if (cnt == 0) {
-                *exists_ptr = 0;
-                return l;
-            }
-
-            cmp = fn(key, l);
-            if (cmp == 0) {
-                *exists_ptr = 1;
-                return l;
-            }
-
-            *exists_ptr = 0;
-            return cmp < 0 ? l : l + size;
-        }
-
-        if ((cnt & 1) == 0) {
-            index--;
-        }
-
-        mid = l + size * index;
-
+    while (l < r) {
+        mid = l + size * ((r - l) / size / 2);
         cmp = fn(key, mid);
+
         if (cmp == 0) {
             *exists_ptr = 1;
             return mid;
-        }
-
-        if (cmp < 0) {
-            r = mid - size;
-            if ((cnt & 1) != 0) {
-                cnt /= 2;
-            } else {
-                cnt /= 2;
-                cnt -= 1;
-            }
+        } else if (cmp < 0) {
+            r = mid;
         } else {
             l = mid + size;
-            cnt /= 2;
         }
     }
 
